7cd69fb33a2e53b5a009bd8e0e7bae232f9df099
am-groce <ambrose.groce@gmail.com>
2025-10-06 18:55:15 -0400

    Update key.h
    added generic template representation

--- key.h
+++ key.h
@@ -2,16 +2,36 @@
 #define PROJECT_1_KEY_H
 
 #include <string>
+#include <cassert>
 
-constexpr int KEY_SIZE = 16;
+//The three supported sized of keys in AES
+enum KEY_SIZES {
+    KEY_128 = 16,   //16 bytes, 128 bits
+    KEY_192 = 24,   //24 bytes, 192 bits
+    KEY_256 = 32,   //32 bytes, 256 bits
+};
 
+template<KEY_SIZES size>
 class Key {
 private:
-    unsigned char key[KEY_SIZE]{};
+    unsigned char key[size]{};
 
 public:
-    std::string printKey();
-    void flipBit(int index);
+    std::string printKey() {    //Outputs key in big endian form
+        std::string output;
+        for (const unsigned char & i : key)
+            output += std::to_string(i) + " ";
+        output += "\n";
+        return output;
+    }
+
+    void flipBit(const int index) {
+        assert(index >= 0);
+        const int byteNo = index / 8;
+        const int bitNo = index % 8;
+
+        key[byteNo] ^= 1 << bitNo;
+    }
 
     Key() {
         for (unsigned char & i : key) { i = 0; }
