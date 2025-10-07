e3577b4ed973991ab8696752d35eed5191fad60d
am-groce <ambrose.groce@gmail.com>
2025-10-07 10:52:17 -0400

    comments
    adjusted white spaces

--- AES_Algorithm.h
+++ AES_Algorithm.h
@@ -6,17 +6,17 @@
 #include <random>
 #include <iostream>
 
-template <KEY_SIZES size>
+template <KEY_SIZES size>   //Defined differently depending on the size of the key
 class AES_Algorithm {
 private:
     Key<size> key;
     BlockConverter blockConverter;
 
-    std::string inputFile;
+    std::string inputFile;  //Input file name
     int numProcessingSteps = 0; //numProcessingSteps is the number of times that step 3 is repeated
 
 public:
-    void init() {
+    void init() {   //Gets input from file and processes it into blocks
         std::cout << "Enter file name: " << std::endl;
         std::cin >> inputFile;
         blockConverter.convertBlocks(inputFile);
@@ -24,17 +24,11 @@ public:
         std::cout << blockConverter.print();
     }
 
-    void keyExpansion() {
+    void keyExpansion() {}
 
-    }
+    void addRoundKey() {}
 
-    void addRoundKey() {
-
-    }
-
-    void subBytes() {
-
-    }
+    void subBytes() {}
 
     void shiftRows() {
         char carry = 0;
@@ -44,7 +38,7 @@ public:
         char carry[4] = {0, 0, 0, 0};
     }
 
-    void encrypt() {
+    void encrypt() {    //Following AES protocols
         addRoundKey();
 
         for (int i = 0; i < numProcessingSteps; i++) {   //9-13 rounds depending on key size
@@ -59,7 +53,7 @@ public:
         addRoundKey();
     }
 
-    void flipRandomBit() {
+    void flipRandomBit() {  //Gets a random index and flips the associated bit
         std::random_device rd;
         std::mt19937 gen(rd());
         std::uniform_int_distribution<> distrib(0, size * 8 - 1);   //Between 0 and bit size - 1
@@ -67,12 +61,11 @@ public:
         key.flipBit(distrib(gen));  //Random bit flipped
     }
 
-    AES_Algorithm() {
+    AES_Algorithm() {   //Default constructor
         if (size == KEY_128) { numProcessingSteps = 9; }
         if (size == KEY_192) { numProcessingSteps = 11; }
         if (size == KEY_256) { numProcessingSteps = 13; }
     };
 };
-
 
 #endif //PROJECT_1_AES_ALGORITHM_H
