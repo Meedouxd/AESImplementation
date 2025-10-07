23e25ad15e8343f16da2f1cf190138a9c07788df
Chase Morgan <cmorgan585@hotmail.com>
2025-10-07 17:11:05 -0400

    file organization

---
+++ python/main.py
@@ -0,0 +1,9 @@
+"""
+Main file for testing the AES encryption algorithm and graphing the results
+Author: Chase Morgan 10/06/25
+"""
+
+input_file:str = ""
+print("****** AES Testing Program ******")
+input_file = input("Enter the location of the plain text file to encrypt:\n")
+
